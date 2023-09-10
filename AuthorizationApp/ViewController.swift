314e0be88b7a405c9bd507ad6a5a9dece3cc9e1d
Egor Ukolov <ukolovegor@yandex.ru>
2023-09-10 21:40:16 +0700

    Initial Commit

---
+++ AuthorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizationApp
+//
+//  Created by Egor Ukolov on 10.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
