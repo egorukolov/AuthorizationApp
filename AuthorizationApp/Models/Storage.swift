16e71cd295bfc10c026f9eee177d4236f240812a
Egor Ukolov <ukolovegor@yandex.ru>
2023-09-26 22:37:11 +0700

    Создание TabBarController, Model

---
+++ AuthorizationApp/Models/Storage.swift
@@ -0,0 +1,11 @@
+//
+//  Storage.swift
+//  AuthorizationApp
+//
+//  Created by Egor Ukolov on 26.09.2023.
+//
+
+struct Storage {
+    let login = ""
+    let password = ""
+}
---
+++ AuthorizationApp/ViewControllers/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  AuthorizationApp
+//
+//  Created by Egor Ukolov on 26.09.2023.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
